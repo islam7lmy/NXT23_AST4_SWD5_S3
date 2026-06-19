d9ece821b5b64ab87ad35eaa44bd8ebc64bbd3ba
islam helmy <islam7lmy@gmail.com>
2026-06-19 12:54:36 +0300

    EFCore

---
+++ EFCore/Contexts/CompanyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Contexts
+{
+    internal class CompanyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Data Source = ISLAM7LMYLAPVM\\ISLAM7LMY; intial Catalog = NXT23.EFMyCompany; Integrated Security = true");
+            //optionsBuilder.UseSqlServer("Data Source = ISLAM7LMYLAPVM\\ISLAM7LMY; intial Catalog = NXT23.EFMyCompany; User Id = sa; Password = your_password");
+            //optionsBuilder.UseSqlServer("Data Source=ISLAM7LMYLAPVM\\ISLAM7LMY;Initial Catalog=NXT23.EFMyCompany;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
+            optionsBuilder.UseSqlServer("Server=ISLAM7LMYLAPVM\\ISLAM7LMY;DataBase=NXT23.EFMyCompany;Trusted_Connection=True;Encrypt=False;");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EFCore/Entities/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Entities
+{
+    #region 1. by Convention (Default Bhavior)
+    internal class Employee
+    {
+        public int Id { get; set; } // primary key by convention int [identity(1,1)] not allow null
+        public string? Name { get; set; } // nvarchar(max) allow null
+        public double Salary { get; set; } // float not allow null
+        public int? Age { get; set; } // int allow null
+    }
+    #endregion
+}
